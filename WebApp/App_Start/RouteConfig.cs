87815009e8f39709ef7faed0fe636cda00684e9a
Aneesha <aneeshaveleri58@gmail.com>
2024-07-29 20:55:07 +0900

    EntityConfigured and Login Page Designed

--- WebApp/App_Start/RouteConfig.cs
+++ WebApp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Admin", action = "login", id = UrlParameter.Optional }
             );
         }
     }
