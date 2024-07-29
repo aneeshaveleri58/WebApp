f7695885a632b8b2b23328c0b546aca749a77e35
Aneesha <aneeshaveleri58@gmail.com>
2024-07-29 12:46:35 -0400

    Create Category and create update view and delete product functions

--- WebApp/Controllers/AdminController.cs
+++ WebApp/Controllers/AdminController.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApp.Models;
-using System.Data.Entity; // Ensure this namespace is included
+using System.Data.Entity;
+using System.Data;
+using System.Data.Entity.Validation;
+using System.Data.Entity.Infrastructure;
 
 namespace WebApp.Controllers
 {
@@ -44,11 +47,32 @@ namespace WebApp.Controllers
         }
 
         // GET: Admin/ManageProducts
+        [HttpGet]
+        public ActionResult CreateProduct()
+        {
+            ViewBag.Categories = _context.tbl_category.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateProduct(tbl_product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.tbl_product.Add(product);
+                _context.SaveChanges();
+                return RedirectToAction("ManageProducts");
+            }
+            ViewBag.Categories = _context.tbl_category.ToList();
+            return View(product);
+        }
+
         public ActionResult ManageProducts()
         {
             var products = _context.tbl_product.ToList();
             return View(products);
         }
+
 
         // GET: Admin/EditProduct/5
         public ActionResult EditProduct(int id)
@@ -63,12 +87,13 @@ namespace WebApp.Controllers
 
         // POST: Admin/EditProduct/5
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult EditProduct(tbl_product product)
         {
             if (ModelState.IsValid)
             {
-                //_context.Entry(product).State = EntityState.Modified; // Ensure this line is correct
-                //_context.SaveChanges();
+                _context.Entry(product).State = EntityState.Modified;
+                _context.SaveChanges();
                 return RedirectToAction("ManageProducts");
             }
             return View(product);
@@ -87,12 +112,117 @@ namespace WebApp.Controllers
 
         // POST: Admin/DeleteProduct/5
         [HttpPost, ActionName("DeleteProduct")]
+        [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
             var product = _context.tbl_product.Find(id);
             _context.tbl_product.Remove(product);
             _context.SaveChanges();
             return RedirectToAction("ManageProducts");
+        }
+
+        // GET: Admin/ManageCategories
+        [HttpGet]
+        public ActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCategory(tbl_category category)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Check if category already exists
+                    bool categoryExists = _context.tbl_category.Any(c => c.cat_name == category.cat_name);
+                    if (categoryExists)
+                    {
+                        ModelState.AddModelError("", "A category with this name already exists.");
+                        return View(category);
+                    }
+
+                    _context.tbl_category.Add(category);
+                    _context.SaveChanges();
+                    return RedirectToAction("ManageCategories");
+                }
+                catch (DbUpdateException ex)
+                {
+                    // Log the exception and handle specific constraint violation
+                    if (ex.InnerException != null && ex.InnerException.Message.Contains("UQ__tbl_cate__FA8C1790195E278D"))
+                    {
+                        ModelState.AddModelError("", "A category with this name already exists.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "An error occurred while saving the category.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "An unexpected error occurred.");
+                }
+            }
+            return View(category);
+        }
+
+
+
+
+
+        public ActionResult ManageCategories()
+        {
+            var categories = _context.tbl_category.ToList();
+            return View(categories);
+        }
+
+
+        // GET: Admin/EditCategory/5
+        public ActionResult EditCategory(int id)
+        {
+            var category = _context.tbl_category.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Admin/EditCategory/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditCategory(tbl_category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(category).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("ManageCategories");
+            }
+            return View(category);
+        }
+
+        // GET: Admin/DeleteCategory/5
+        public ActionResult DeleteCategory(int id)
+        {
+            var category = _context.tbl_category.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Admin/DeleteCategory/5
+        [HttpPost, ActionName("DeleteCategory")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteCategoryConfirmed(int id)
+        {
+            var category = _context.tbl_category.Find(id);
+            _context.tbl_category.Remove(category);
+            _context.SaveChanges();
+            return RedirectToAction("ManageCategories");
         }
     }
 }
